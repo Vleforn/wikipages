a70159b1c844ebbdbfc7c22c5ee0b8f38b2cdc64
vleforn <vleforn@gmail.com>
2026-06-04 23:11:36 +0500

    ci: databricks related resources

--- infra/main.tf
+++ infra/main.tf
@@ -1,27 +1,59 @@
-resource "azurerm_resource_group" "smoketest" {
-  name     = "rg-smoketest-dev-weu-001"
+# Azure resources
+resource "azurerm_resource_group" "wikipages" {
+  name     = "rg-wikipages-dev-weu-001"
   location = "westeurope"
-
-  tags = {
-    purpose = "terraform-smoketest"
-    managed = "terraform"
-  }
 }
 
-resource "azurerm_resource_group" "dysonschwinger" {
-  name     = "rg-dysonschwinger-dev-weu-001"
-  location = "westeurope"
-
-  tags = {
-    purpose = "terraform-dysonschwinger"
-    managed = "terraform"
-  }
+resource "azurerm_storage_account" "wikipages" {
+  name = "stwpvleforndevweu001"
+  location = azurerm_resource_group.wikipages.location
+  resource_group_name = azurerm_resource_group.wikipages.name
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+  is_hns_enabled = true
 }
 
-output "resource_group_id" {
-  value = azurerm_resource_group.smoketest.id
+# TODO: Connect containers to databricks as external locations
+resource "azurerm_storage_container" "bronze" {
+  name = "bronze"
+  storage_account_id = azurerm_storage_account.wikipages.id
 }
 
-output "resource_group_dysonschwinger_id" {
-  value = azurerm_resource_group.smoketest.id
+resource "azurerm_storage_container" "silver" {
+  name = "silver"
+  storage_account_id = azurerm_storage_account.wikipages.id
 }
+
+resource "azurerm_storage_container" "gold" {
+  name = "gold"
+  storage_account_id = azurerm_storage_account.wikipages.id
+}
+
+resource "azurerm_databricks_workspace" "wikipages" {
+  name = "adb-wikipages-dev-weu-001"
+  location = azurerm_resource_group.wikipages.location
+  resource_group_name = azurerm_resource_group.wikipages.name
+  sku = "Standard"
+}
+
+# # TODO: keyvault configuration
+# resource "azurerm_key_vault" "wikipages" {
+#   name = "kv-wikipages-dev-weu-001"
+#   location = azurerm_resource_group.wikipages.location
+#   resource_group_name = azurerm_resource_group.wikipages.name
+#   tenant_id = "???"
+#   sku_name = "???"
+# }
+
+
+# Databricks resources
+
+# TODO: Unity Catalog???
+
+# output "resource_group_id" {
+#   value = azurerm_resource_group.smoketest.id
+# }
+
+# output "resource_group_dysonschwinger_id" {
+#   value = azurerm_resource_group.smoketest.id
+# }
